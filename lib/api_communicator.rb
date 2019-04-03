ce2d381650ca14563aa96392bf263e47ba681d65
Robert <r_lee94@yahoo.com>
2019-04-03 13:49:04 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
+ 
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
